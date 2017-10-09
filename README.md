41f6fca319b3c24c14ab24626e90f248af8f85e0
Fernando Barbeiro <fernandoabcampos@gmail.com>
2017-10-09 22:56:18 +0200

    Regression - loading first dataset

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # machine-learning-python-basics
+![Regression dataset](https://raw.githubusercontent.com/fernandoabcampos/machine-learning-python-basics/master/resources/regression.png)
--- loading/regression.py
+++ loading/regression.py
@@ -2,7 +2,7 @@ from sklearn.datasets import make_regression
 
 X, y, coef = make_regression(n_samples=100,
                              n_features=3,
-                             n_informative=3,
+                             n_informative=3, #number of features used to generate the target value
                              n_targets=1,
                              noise=0.0,
                              coef=True,
