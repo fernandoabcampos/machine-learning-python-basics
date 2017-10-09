d7c731359b93e41a2ba4745a5b7d08f672d6ce8f
Fernando Barbeiro <fernandoabcampos@gmail.com>
2017-10-09 22:45:44 +0200

    make_regression (Supervised Learning: predicts from labeled data as a TARGET value -> Regression: target values are continuous values.

---
+++ loading/regression.py
@@ -0,0 +1,13 @@
+from sklearn.datasets import make_regression
+
+X, y, coef = make_regression(n_samples=100,
+                             n_features=3,
+                             n_informative=3,
+                             n_targets=1,
+                             noise=0.0,
+                             coef=True,
+                             random_state=1)
+
+# View feature matrix and target vector
+print('Feature Matrix\n', X[:3])
+print('Target Vector\n', y[:3])
